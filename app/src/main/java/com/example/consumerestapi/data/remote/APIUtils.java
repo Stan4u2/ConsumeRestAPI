11a4608d9d1d8527b25f9e43668b298ed6d794cb
Jose <ktperry1980@gmail.com>
2019-09-04 07:42:10 -0500

    yeah

--- app/src/main/java/com/example/consumerestapi/data/remote/APIUtils.java
+++ app/src/main/java/com/example/consumerestapi/data/remote/APIUtils.java
@@ -3,7 +3,7 @@ package com.example.consumerestapi.data.remote;
 public class APIUtils {
 
     private APIUtils (){
-        public static final String BASE_URL =
+        public static final String BASE_URL ="yeah";
     }
 
 }
